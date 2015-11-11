ef90a392e6d50ead224b98abca1556dccbabc1e6
Fletcher <fletcherc89@gmail.com>
2015-11-11 11:55:42 -0800

    Constellations timed appearance

---
+++ Assets/ForAshley/Scripts/VisibilityProgressRange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class VisibilityProgressRange : SceneProgressElement
+{
+    public float VisibilityStart = 0.0f;
+    public float Duration = 1.0f;
+
+    protected override void UpdateProgress(float progress)
+    {
+        base.UpdateProgress(progress);
+        this.gameObject.SetActive(progress >= this.VisibilityStart && progress <= this.VisibilityStart + this.Duration);
+    }
+}
