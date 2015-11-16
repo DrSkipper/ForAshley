511596e65598fcd0d546dc9474bcab46994484b4
Fletcher <fletcherc89@gmail.com>
2015-11-15 23:54:42 -0800

    Transition, sound and build work

--- Assets/ForAshley/Scripts/SceneTransitions/CampfireEndTransition.cs
+++ Assets/ForAshley/Scripts/SceneTransitions/CampfireEndTransition.cs
@@ -2,5 +2,18 @@
 
 public class CampfireEndTransition : TransitionHandler
 {
+    public GameObject ShootingStars;
+    public GameObject ClosingTrack;
 
+    public override void HandleLockEntered()
+    {
+        base.HandleLockEntered();
+        ShootingStars.GetComponent<ParticleSystem>().Play();
+    }
+
+    public override void HandleLockOver()
+    {
+        base.HandleLockOver();
+        ClosingTrack.GetComponent<AudioSource>().Play();
+    }
 }
